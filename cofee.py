aeb8a20148177fe778c8bfe38b607c127cd7e4a1
pornyhunter <101872035+pornyhunter@users.noreply.github.com>
2023-03-28 07:36:22 +0530

    First code
    Hey gooys

---
+++ cofee.py
@@ -0,0 +1,20 @@
+
+
+name = input(" What is your name ? \n")
+
+menu = ("coffee , green tea, juice")
+
+print("Hello " + name + " what would you like to have?" + " This is our menu : \n\n" + menu)
+
+order = input()
+
+print("Thank you " + order + " is on its way")
+
+price = 8
+
+quantity = input("How many coffes do you like? \n")
+
+Result = price * int(quantity)
+
+
+print("your total price is \n" + str(Result))
