764afdb2a46f5d087298cbaaed249140bf9fe144
pornyhunter <101872035+pornyhunter@users.noreply.github.com>
2023-03-29 11:29:54 +0530

    new edition in code
     A small description

--- cofee.py
+++ cofee.py
@@ -4,17 +4,37 @@ name = input(" What is your name ? \n")
 
 menu = ("coffee , green tea, juice")
 
-print("Hello " + name + " what would you like to have?" + " This is our menu : \n\n" + menu)
+if name == "Alen":
+    answer = input(" Hey " + name + " are you evil \n")
 
-order = input()
+    if answer == "yes":
+        print("Sorry, You are not welcone here ..!")
+        exit()
+    elif answer == "no":
+        print("Hello " + name +  " Welcome, Come on in")
 
-print("Thank you " + order + " is on its way")
+else:
+    print("Hello " + name + " Please come in \n")
+order = input(" \nThis is our menu : \n" + menu + " \n\n What would you like to have? \n")
 
-price = 8
+if order == "falloda":
+    price = 16
+elif order == "coffee":
+    price = 8 
+elif order == "green tea": 
+    temp = input(" Do you want it hot or cold ? \n")
+    if temp == "hot":
+        price = 12
+    elif temp == "cold":
+        price = 14
+elif order =="juice":
+    price = 10
+else:
+    print(" Sorry we don't currently have that....")
 
-quantity = input("How many coffes do you like? \n")
+quantity = input("How many " + order + " do you like? \n  ")
 
 Result = price * int(quantity)
+print("Thank you, your " + order + " is on its way and " + "your total price is:      $" + str(Result))
 
-
-print("your total price is \n" + str(Result))
+exit()
